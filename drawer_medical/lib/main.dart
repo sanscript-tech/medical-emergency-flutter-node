1f2a6f787189069f88ca639e565b773e800e2307
Geeky-star <nimratkaur540@gmail.com>
2020-10-27 13:01:30 +0530

    added improved files

---
+++ drawer_medical/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:drawer_medical/pages/drawer.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
