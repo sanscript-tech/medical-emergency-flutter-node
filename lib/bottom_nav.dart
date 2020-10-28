572faac2271d39688653dd9bc4a09c94b0ef24a5
samflab <kasturimeh@gmail.com>
2020-10-28 11:00:50 +0530

    Bottom nav bar

---
+++ lib/bottom_nav.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:medicare/icons/icons.dart';
+import 'home.dart';
+import 'shop.dart';
+import 'doctor.dart';
+import 'hospital.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+//this page contains a common bottom navigation bar for all the pages.
+class BottomNav extends StatefulWidget {
+  @override
+  _BottomNavState createState() => _BottomNavState();
+}
+
+class _BottomNavState extends State<BottomNav> {
+  //setting the current index at 0
+  int _currentIndex = 0;
+  //the order of these widgets should be kept same as the bottom navigation bar
+  final List<Widget> _tabs = [Home(), Hospital(), Doctor(), Shop()];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey.shade200,
+      body: _tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.orange,
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: new FaIcon(
+                MyFlutterApp.home,
+                color: Colors.orange.shade50,
+                size: 25.0,
+              ),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                'Home',
+                style: TextStyle(
+                  color: Colors.orange.shade50,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: FaIcon(
+                MyFlutterApp.hospital,
+                color: Colors.orange.shade50,
+                size: 25.0,
+              ),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                "Hospital",
+                style: TextStyle(
+                  color: Colors.orange.shade50,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: FaIcon(
+                MyFlutterApp.doctor,
+                color: Colors.orange.shade50,
+                size: 25.0,
+              ),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                "Doctor",
+                style: TextStyle(
+                  color: Colors.orange.shade50,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: FaIcon(
+                MyFlutterApp.medicine,
+                color: Colors.orange.shade50,
+                size: 25.0,
+              ),
+            ),
+            title: Padding(
+              padding: const EdgeInsets.only(top: 5.0),
+              child: Text(
+                "Shop",
+                style: TextStyle(
+                  color: Colors.orange.shade50,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
