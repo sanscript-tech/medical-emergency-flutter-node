83a9a118c6ede27541b094b4d8bd5aad5e0cf7af
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2020-10-29 04:01:07 +0530

    feat: login route added

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,5 +27,55 @@ const userSchema = new mongoose.Schema({
     },
   ],
 });
+
+//middleware hash the plain text password before saving
+userSchema.pre('save', async function(next){
+
+  if(this.isModified('password')){
+      this.password = await bcrypt.hash(this.password, 8);
+  }
+
+  next()
+})
+
+// used to remove private data from JSON response
+userSchema.methods.toJSON = function() {      
+  const userObject = this.toObject();
+
+  delete userObject.password;
+  delete userObject.tokens;
+
+  return userObject;
+}
+
+// creates and saves a jwt token to the user record and returns the token
+userSchema.methods.generateAuthToken = async function() {                     
+  const token = jwt.sign({_id: this._id.toString() }, process.env.JWT_SECRET, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+  })
+
+  this.tokens = this.tokens.concat({ token });
+  await this.save();
+
+  return token;
+}
+
+// searches for a user using email and password
+userSchema.statics.findByCredentials = async(email, password) => {         
+  const user = await User.findOne({email})
+  
+  if(!user){
+      throw new Error({"error": "Unable to login"});
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch){
+      throw new Error({"error": "Unable to login"});
+  }
+
+  return user;
+}
+
+
 
 module.exports = mongoose.model("user", userSchema);
--- server/src/routers/api/userRouter.js
+++ server/src/routers/api/userRouter.js
@@ -1,9 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const User = require('../../models/user');
+
+
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
+
+router.post("/login", async (req,res) => {
+  const user = new User(req.body)
+    try{
+        await user.save();
+        const token = await user.generateAuthToken();
+        const filtered_user = user.toJSON();
+        res.status(201).json({msg: "You have been logged in successfully",user:filtered_user, token});
+    }catch(e){
+        res.status(400).send({ errors: [{msg: "Unable to login"}] });
+    } 
+})
 
 module.exports = router;
