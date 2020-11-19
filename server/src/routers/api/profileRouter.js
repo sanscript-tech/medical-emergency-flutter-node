1a0292af7be983e4aa6b72fec9a41867ccd6596f
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2020-11-19 23:19:58 +0530

    feat: Create/Update profile route added

--- server/src/routers/api/profileRouter.js
+++ server/src/routers/api/profileRouter.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 const UserProfile = require("../../models/Profile");
-const auth = require("../../middleware/auth")
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/user");
 
 router.get("/", async(req,res)=> {
     try{
@@ -35,15 +37,29 @@ router.get("/:id", async(req,res)=> {
 
 
 router.post("/me" , auth, async (req, res) => {
-  req.body.userField = req.user._id;
-  const profile = new UserProfile(req.body)
+  
   try{
+
+    var profile = await Profile.find({userField: req.user._id}).populate()
+    if(profile.length==1){
+      const updates = Object.keys(req.body)
+      updates.forEach((update)=> req.user[update]=req.body[update])
+      await req.user.save();
+      res.send({msg: "Your profile has been updated successfully",user: req.user});
+
+    }else{
+
+      req.body.userField = req.user._id;
+      const profile = new UserProfile(req.body)
       await profile.populate().save();
       res.status(201).json({msg: "Your account has been created successfully",profile})
+
+    }
   }catch(e){
       console.log(e);
       res.status(400).send({ errors: [{msg: e.message}] });
   } 
+  
 })
 
 router.delete('/me', auth, async(req,res)=>{
