41de55a0ac084809e59a5dd99d4887be2ca89563
Manthan Gupta <manthangupta109@gmail.com>
2020-11-20 15:09:51 +0530

    Medicine Detail Doctor Side

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:medicare/pages/doctor%20side/doctor_confirmation.dart';
+import 'package:medicare/pages/doctor%20side/medicine_details.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: DoctorConfirmation(),
+      home: MedicineDetailsDoctor(),
     );
   }
 }
